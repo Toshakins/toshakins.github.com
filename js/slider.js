eb3d168f3c5e353f84d8b499f173f117be92e4ec
Toshakins <toshakins@gmail.com>
2012-12-17 23:10:53 +0200

    bugfixes

--- js/slider.js
+++ js/slider.js
@@ -4,13 +4,6 @@ $.fn.slider = function () {
         $slider = $wrapper.find('> ul'),
         $items = $slider.find('> li');
         var currentPage = 0;
-
-        function gotoPage (page, currentPage) {
-            if (page != currentPage) {
-
-            }
-        }
-
         $wrapper.after('<div class="selection"></div>');
         var $selection = $('.selection');
         $selection.append('<ul></ul>');
@@ -22,7 +15,7 @@ $.fn.slider = function () {
         var $selectors =  $selectorBox.find('> li');
         $selectors.each(function (index) {
             $(this).click(function () {
-                var choice = $(this).find('>a').attr('href').match('[^#/]+$');
+                var choice = parseInt($(this).find('>a').attr('href').match('[^#/]+$'));
                 if (choice != currentPage) {
                     $items.eq(currentPage).fadeOut("slow");
                     $items.eq(choice).fadeIn("slow");
