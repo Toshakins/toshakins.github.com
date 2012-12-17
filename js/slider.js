464c810e7ca21b44ccdf4ff3ce826153b291cc88
Toshakins <toshakins@gmail.com>
2012-12-17 22:47:35 +0200

    slider functionality ready

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,38 @@
 $.fn.slider = function () {
-	return this.each(function() {
-		var $wrapper = $('> div', this).css('overflow', 'hidden'),
-		$slider = $wrapper.find('> ul'),
-		$items = $slider.find(' > li'),
-		$single = $items.find(':first');
+    return this.each(function() {
+        var $wrapper = $('> div', this).css('overflow', 'hidden'),
+        $slider = $wrapper.find('> ul'),
+        $items = $slider.find('> li');
+        var currentPage = 0;
 
-		$wrapper.after('<div class="selection"></div>');
-		var $selection = $('.selection');
-		$selection.append('<ul></ul>');
-		var $selectors = $('.selection').find('> ul');
-		$items.each(function (index) {
-			$selectors.append('<li><a href=#' + index + '></a></li>')
-		});
-		
-	});
+        function gotoPage (page, currentPage) {
+            if (page != currentPage) {
+
+            }
+        }
+
+        $wrapper.after('<div class="selection"></div>');
+        var $selection = $('.selection');
+        $selection.append('<ul></ul>');
+        $selectorBox = $selection.find('> ul');
+        $items.each(function (index) {
+            $selectorBox.append('<li><a href=#' + 
+                ($items.length - index - 1) + '></a></li>');
+        });
+        var $selectors =  $selectorBox.find('> li');
+        $selectors.each(function (index) {
+            $(this).click(function () {
+                var choice = $(this).find('>a').attr('href').match('[^#/]+$');
+                if (choice != currentPage) {
+                    $items.eq(currentPage).fadeOut("slow");
+                    $items.eq(choice).fadeIn("slow");
+                    currentPage = choice;
+                };
+            });
+        });
+    });
 };
 
-$(document).ready(function () {
+$(function () {
   $('.slider').slider();
 });
