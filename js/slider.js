275b2942ad77e64239154390cada8bddf60afffd
Toshakins <toshakins@gmail.com>
2012-12-19 17:56:43 +0200

    selector selecting works

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,7 @@
 $.fn.slider = function (slideDelay) {
     return this.each(function() {
         var $wrapper = $('> div', this).css('overflow', 'hidden'),
-        $slider = $wrapper.find('> ul'),
-        $items = $slider.find('> li');
+        $items = $wrapper.find('li');
         var currentPage = 0;
         $wrapper.after('<div class="selection"></div>');
         var $selection = $('.selection');
@@ -13,23 +12,32 @@ $.fn.slider = function (slideDelay) {
                 ($items.length - index - 1) + '></a></li>');
             $(this).css('display', 'none');
         });
+
+        function nextFrame (choice) {
+            //getter hack cause of <li> output in inproper order
+            $selectors.eq($items.length - currentPage - 1).removeClass('selected');
+            $selectors.eq($items.length - choice - 1).addClass('selected');
+            $items.eq(currentPage).fadeOut("slow");
+            $items.eq(choice).fadeIn("slow");
+            currentPage = choice;
+        }
+
         function player() {
             return setInterval((function () {
-                $items.eq(currentPage).fadeOut("slow");
-                $items.eq(currentPage = (++currentPage % $items.length)).fadeIn("slow");
+                nextFrame((currentPage + 1) % $items.length);
             }), slideDelay);
         }
         var stop = player();
         $items.eq(currentPage).css('display', 'list-item');
         var $selectors =  $selectorBox.find('> li');
+        //misordered ouptup of <li>, that's why hack
+        $selectors.eq($items.length - currentPage - 1).addClass('selected');
         $selectors.each(function (index) {
             $(this).click(function () {
                 clearInterval(stop);
                 var choice = parseInt($(this).find('>a').attr('href').match('[^#/]+$'));
                 if (choice != currentPage) {
-                    $items.eq(currentPage).fadeOut("slow");
-                    $items.eq(choice).fadeIn("slow");
-                    currentPage = choice;
+                    nextFrame(choice);
                     $(this).delay(slideDelay);
                 };
                 stop = player();
@@ -39,5 +47,5 @@ $.fn.slider = function (slideDelay) {
 };
 
 $(function () {
-  $('.slider').slider(5000);
+  $('.slider').slider(2000);
 });
