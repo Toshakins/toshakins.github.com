17220557ba182f74b134ff8bbc39372fff1c5be9
Toshakins <toshakins@gmail.com>
2012-12-20 22:12:04 +0200

    milestone

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,8 @@
 $.fn.slider = function (slideDelay) {
     return this.each(function() {
         var $wrapper = $('> div', this).css('overflow', 'hidden'),
-        $items = $wrapper.find('li');
+        $itemsBox = $wrapper.find('> ul');
+        $items = $itemsBox.find('> li');
         var currentPage = 0;
         $wrapper.after('<div class="selection"></div>');
         var $selection = $('.selection');
@@ -12,35 +13,34 @@ $.fn.slider = function (slideDelay) {
                 ($items.length - index - 1) + '></a></li>');
             $(this).css('display', 'none');
         });
-
+        //save wrapper params for IE8
         function nextFrame (choice) {
-            //getter hack cause of <li> output in inproper order
+            //eq hack cause of <li> output inproper order
             $selectors.eq($items.length - currentPage - 1).removeClass('selected');
             $selectors.eq($items.length - choice - 1).addClass('selected');
             $items.eq(currentPage).fadeOut("slow");
             $items.eq(choice).fadeIn("slow");
             currentPage = choice;
-        }
-
+        };
         function player() {
             return setInterval((function () {
                 nextFrame((currentPage + 1) % $items.length);
             }), slideDelay);
-        }
-        var stop = player();
+        };
+        var breaker = player();
         $items.eq(currentPage).css('display', 'list-item');
         var $selectors =  $selectorBox.find('> li');
-        //misordered ouptup of <li>, that's why hack
+        //misordered output of <li>, that's why mysterious eq
         $selectors.eq($items.length - currentPage - 1).addClass('selected');
         $selectors.each(function (index) {
             $(this).click(function () {
-                clearInterval(stop);
+                clearInterval(breaker);
                 var choice = parseInt($(this).find('>a').attr('href').match('[^#/]+$'));
                 if (choice != currentPage) {
                     nextFrame(choice);
                     $(this).delay(slideDelay);
                 };
-                stop = player();
+                breaker = player();
             });
         });
     });
