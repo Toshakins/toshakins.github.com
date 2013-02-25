a30ea70ac4963c1c0b13c7cad9d703299388ef9f
Toshakins <toshakins@gmail.com>
2013-02-26 00:40:40 +0200

    bug solved

--- js/slider.js
+++ js/slider.js
@@ -17,8 +17,7 @@ $.fn.slider = function (slideDelay) {
             //eq hack cause of <li> output inproper order
             $selectors.eq($items.length - currentPage - 1).removeClass('selected');
             $selectors.eq($items.length - choice - 1).addClass('selected');
-            $items.eq(currentPage).fadeOut("slow");
-            $items.eq(choice).fadeIn("slow");
+            $items.eq(currentPage).fadeOut("slow", (function() {$items.eq(choice).fadeIn("slow");}));
             currentPage = choice;
         };
         function player() {
